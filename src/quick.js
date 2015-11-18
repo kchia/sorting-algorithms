8112ea9cc0037502e2cb0168af5d075bfdd4345d
Hou Chia <kchia87@gmail.com>
2015-11-18 12:58:53 -0800

    Implements quicksort

--- src/quick.js
+++ src/quick.js
@@ -10,6 +10,53 @@
  *    Combines the two arrays and the pivot into a sorted array.
  */
 
+var swap = function(array,index1,index2) {
+	var temp = array[index1];
+	array[index1] = array[index2];
+	array[index2] = temp;
+	return array;
+};
+
+var partition = function(arr, left, right) {
+	var pivot = arr[Math.floor((right + left) / 2)];
+	var i = left;
+	var j = right;
+
+	while (i <= j) {
+		while(arr[i] < pivot) {
+			i++;
+		}
+		while(arr[j] > pivot) {
+			j--;
+		}
+		if( i <= j){
+			swap(arr, i, j);
+			i++;
+			j--;
+		}
+	}
+
+	return i;
+};
+
+var quickSort = function(array, left, right) {
+	var index = partition(array, left, right);
+
+	if(array.length < 2) {
+		return array;
+	}
+
+	if(left < index -1){
+		quickSort(array, left, index-1);
+	}
+
+	if(index < right) {
+		quickSort(array, index, right);
+	}
+
+	return array;
+};
+
 var quicksort = function(array) {
-  
+	return quickSort(array, 0, array.length-1)
 };
